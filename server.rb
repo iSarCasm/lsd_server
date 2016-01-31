79e79649556e3c7016d3b7a2a3886074cb1cccf5
SarCasm <sarcasm008@gmail.com>
2016-01-31 20:26:40 +0200

    sddsfdfs

--- server.rb
+++ server.rb
@@ -72,7 +72,7 @@ class Server
       begin
         msg = client.recv(100)
         got_message(msg.force_encoding('UTF-8'), client) if (msg != nil)
-      # rescue Exception => e
+      rescue Exception => e
         ap "#{Thread.current[:client].ip} disconnected."
         @connections[:clients].delete(Thread.current[:client])
         @online -= 1
